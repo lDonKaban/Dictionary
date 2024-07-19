7b289708e4a7357e49c8e9c56fb207cce7153c5a
erzinap <ErzinAP@kamaz.ru>
2024-07-19 22:24:48 +0300

    refactor code (added array with elements for component TermList) :recycle:

--- src/TermList.jsx
+++ src/TermList.jsx
@@ -1,25 +1,30 @@
 import { TermCard } from "./TermCard.jsx";
 import './TermList.css'
+
+const terms = [
+    {
+        title: 'React',
+        description: 'Библиотека для создания пользовательских интерфейсов',
+    },
+    {
+        title: 'React-компонент',
+        description: 'Функция, которая возвращает React-элемент',
+    },
+    {
+        title: 'Render',
+    },
+]
 export const TermList = () => {
     return (
         <ul className='term-list'>
-            <li>
-                <TermCard
-                    title='React'
-                    description='Библиотека для создания пользовательских интерфейсов'
-                />
-            </li>
-            <li>
-                <TermCard
-                    title='React-компонент'
-                    description='Функция, которая возвращает React-элемент'
-                />
-            </li>
-            <li>
-                <TermCard
-                    title='Render'
-                />
-            </li>
+            {terms.map((item) => (
+                <li>
+                    <TermCard
+                        title={item.title}
+                        description={item.description}
+                    />
+                </li>
+            ))}
         </ul>
     )
 }
