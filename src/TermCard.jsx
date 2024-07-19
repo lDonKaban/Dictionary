b268d10ed329117543e4817ebd579cca9738e5b7
erzinap <ErzinAP@kamaz.ru>
2024-07-19 23:39:59 +0300

    rtfactor code :recycle:

--- src/TermCard.jsx
+++ src/TermCard.jsx
@@ -2,8 +2,8 @@ import './TermCard.css'
 export const TermCard = ({ title, description }) => {
     return (
         <div className='term-card'>
-            <h2 className='term-card_title'>{title}</h2>
-            {description && <p className='term-card_description'>{description}</p>}
+            <h2 className='term-card__title'>{title}</h2>
+            {description && <p className='term-card__description'>{description}</p>}
         </div>
-    )
-}
+    );
+};
--- src/TermList.jsx
+++ src/TermList.jsx
@@ -16,5 +16,5 @@ export const TermList = ({ terms }) => {
                 </li>
             ))}
         </ul>
-    )
-}
+    );
+};
